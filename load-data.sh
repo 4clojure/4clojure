0482ac4a4b86959a73c1192b8f00f04ed1d8513d
David Byrne <david.r.byrne@gmail.com>
2011-04-19 01:24:24 -0400

    File name fixed

--- load-data.sh
+++ load-data.sh
@@ -1,3 +1,3 @@
 project_classpath=`lein classpath`
 echo $project_classpath
-java -cp $project_classpath  clojure.main ./src/foreclojure/data-set.clj
+java -cp $project_classpath  clojure.main ./src/foreclojure/data_set.clj
