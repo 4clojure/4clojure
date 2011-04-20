44e2d4e6c239927cfeb4814bb49dc10e523a69ef
Raynes <i@raynes.me>
2011-04-19 22:41:32 -0500

    Don't depend on dependencies to depend on your dependencies for you.

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
                  [org.clojars.christophermaier/congomongo "0.1.4-SNAPSHOT"]
 		 [org.jasypt/jasypt "1.7"]
                  [amalloy/utils "[0.3.7,)"]
-                 [clj-github "1.0.0-SNAPSHOT"]]
+                 [clj-github "1.0.0-SNAPSHOT"]
+                 [ring "0.3.7"]]
   :dev-dependencies [[lein-ring "0.4.0"]
                      [swank-clojure "1.2.1"]]
   :main foreclojure.core
