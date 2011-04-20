8ea49dd2c6efd87e3bafadbac7338fd0cad0af34
David Byrne <david.r.byrne@gmail.com>
2011-04-20 10:12:17 -0400

    Secret test added to max value problem

--- src/foreclojure/data_set.clj
+++ src/foreclojure/data_set.clj
@@ -379,4 +379,5 @@
             :description "Write a function which takes a variable number of parameters and returns the maximum value."
             :tags ["easy" "core-functions"]
             :tests ["(= (__ 1 8 3 4) 8)"
-		    "(= (__ 30 20) 30)"]})))
+		    "(= (__ 30 20) 30)"]
+	    :secret-tests ["(= (__ 45 67 11) 67)"]})))
