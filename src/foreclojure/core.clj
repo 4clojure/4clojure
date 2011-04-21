24cb3dd33e8ad2ccec59aeea98ff17bda273b08d
Raynes <i@raynes.me>
2011-04-21 18:51:00 -0500

    Add a configuration file and code to read that file. If :wrap-reload is present and true in the config file, use wrap-reload.

--- src/foreclojure/core.clj
+++ src/foreclojure/core.clj
@@ -3,7 +3,8 @@
         [foreclojure static problems login register users]
         ring.adapter.jetty
         somnium.congomongo
-        [ring.middleware.reload :only [wrap-reload]])
+        [ring.middleware.reload :only [wrap-reload]]
+        [clojure.java.io :only [file]])
   (:require [compojure [route :as route] [handler :as handler]]
             [sandbar.stateful-session :as session]
             [ring.util.response :as response]))
@@ -23,10 +24,17 @@
   (route/resources "/")
   (route/not-found "Page not found"))
 
+(def config
+  (binding [*read-eval* false]
+    (read-string
+     (slurp (file (System/getProperty "user.dir") "config.clj")))))
+
 (def app
   (handler/site
    (session/wrap-stateful-session
-    #'main-routes)))
+    (if (:wrap-reload config)
+      (wrap-reload #'main-routes '(foreclojure.core))
+      #'main-routes))))
 
 (defn run []
   (run-jetty (var app) {:join? false :ssl? true :port 8080 :ssl-port 8443
