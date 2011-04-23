b2e981e1557a665a6845925e2c698de8880c51f7
David Byrne <david.r.byrne@gmail.com>
2011-04-23 19:11:32 -0400

    Retag "Longest Increasing Sub-Seq"

--- src/foreclojure/data_set.clj
+++ src/foreclojure/data_set.clj
@@ -534,7 +534,7 @@
             :title "Longest Increasing Sub-Seq"
             :times-solved 0
             :description "Given a vector of integers, find the longest consecutive sub-sequence of increasing numbers. If two sub-sequences have the same length, use the one that occurs first."
-            :tags ["medium" "seqs"]
+            :tags ["hard" "seqs"]
             :tests ["(= (__ [1 0 1 2 3 0 4 5]) [0 1 2 3])"
 		    "(= (__ [5 6 1 3 2 7]) [5 6])"
 		    "(= (__ [2 3 3 4 5]) [3 4 5])"
