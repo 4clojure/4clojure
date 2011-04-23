5ef016849986c96c712d010303208401ca5b6439
David Byrne <david.r.byrne@gmail.com>
2011-04-23 12:10:08 -0400

    Problem added - Longest Increasing Sub-Seq

--- src/foreclojure/data_set.clj
+++ src/foreclojure/data_set.clj
@@ -527,11 +527,15 @@
             :times-solved 0
             :description "Let bindings and function parameter lists support destructuring."
             :tags ["easy" "destructuring"]
-            :tests ["(= [2 4] (let [[a b c d e f g] (range)] __))"]})))
+            :tests ["(= [2 4] (let [[a b c d e f g] (range)] __))"]})
 
-
-(doseq [{id :_id :keys [test secrets]}
-        (fetch :problems)]
-  (update! :problems
-           {:_id id}
-           {:$set {:test (concat test secrets) :secrets []}}))
+      (insert! :problems
+           {:_id 53
+            :title "Longest Increasing Sub-Seq"
+            :times-solved 0
+            :description "Given a vector of integers, find the longest consecutive sub-sequence of increasing numbers. If two sub-sequences have the same length, use the one that occurs first."
+            :tags ["medium" "seqs"]
+            :tests ["(= (__ [1 0 1 2 3 0 4 5]) [0 1 2 3])"
+		    "(= (__ [5 6 1 2 6 7]) [5 6])"
+		    "(= (__ [2 3 3 4 5]) [3 4 5])"
+		    "(= (__ [7 6 5 4]) [])"]})))
