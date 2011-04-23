6a8f7437cf86f9a254e83adf1f3d9506eb4beec3
David Byrne <david.r.byrne@gmail.com>
2011-04-23 13:12:29 -0400

    Fixed test case bug

--- src/foreclojure/data_set.clj
+++ src/foreclojure/data_set.clj
@@ -536,6 +536,6 @@
             :description "Given a vector of integers, find the longest consecutive sub-sequence of increasing numbers. If two sub-sequences have the same length, use the one that occurs first."
             :tags ["medium" "seqs"]
             :tests ["(= (__ [1 0 1 2 3 0 4 5]) [0 1 2 3])"
-		    "(= (__ [5 6 1 2 6 7]) [5 6])"
+		    "(= (__ [5 6 1 3 2 7]) [5 6])"
 		    "(= (__ [2 3 3 4 5]) [3 4 5])"
 		    "(= (__ [7 6 5 4]) [])"]})))
