790998603511a69b2d84404f04e3cbef7a4ede13
David Byrne <david.r.byrne@gmail.com>
2011-04-24 01:15:57 -0400

    Hint added to the first problem

--- src/foreclojure/data_set.clj
+++ src/foreclojure/data_set.clj
@@ -8,7 +8,7 @@
              {:_id 1
               :title "Nothing but the Truth"
               :times-solved 0
-              :description "This is a clojure form.  Enter a value which will make the form evaluate to true.  Don't over think it!  If you are confused, see the <a href='/directions'>getting started</a> page."
+              :description "This is a clojure form.  Enter a value which will make the form evaluate to true.  Don't over think it!  If you are confused, see the <a href='/directions'>getting started</a> page.  Hint: true is equal to true."
               :tags ["elementary"]
               :tests ["(= __ true)"]})
 
