39f5c7d3981a4db7bbe3c4265e2c81d1d4f50bc4
David Byrne <david.r.byrne@gmail.com>
2011-04-25 21:34:37 -0400

    Updated description for "Sequence reductions"

--- src/foreclojure/data_set.clj
+++ src/foreclojure/data_set.clj
@@ -621,7 +621,7 @@
             :title "Sequence Reductions"
             :times-solved 0
             :restricted ["reductions"]
-            :description "Write a function which behaves like reduce, but returns each intermediate value of the reduction."
+            :description "Write a function which behaves like reduce, but returns each intermediate value of the reduction.  Your function must accept either two or three arguments, and the return sequence must be lazy."
             :tags ["medium" "seqs" "core-functions"]
             :tests ["(= (take 5 (__ + (range))) [0 1 3 6 10])"
                     "(= (__ conj [1] [2 3 4]) [[1] [1 2] [1 2 3] [1 2 3 4]])"
