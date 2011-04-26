31830e87cbab02d592ecdf98fc84d1528730079c
Michael Kohl <citizen428@gmail.com>
2011-04-27 00:08:04 +0200

    Call the defined function

--- src/foreclojure/data_set.clj
+++ src/foreclojure/data_set.clj
@@ -637,3 +637,5 @@
             :tests ["(= (__ [:a :b :c] [1 2 3]) {:a 1, :b 2, :c 3})"
                     "(= (__ [1 2 3 4] [\"one\" \"two\" \"three\"]) {1 \"one\", 2 \"two\", 3 \"three\"})"
                     "(= (__ [:foo :bar] [\"foo\" \"bar\" \"baz\"]) {:foo \"foo\", :bar \"bar\"})"]})))
+
+(load-problems)
