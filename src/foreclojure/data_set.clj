87fdd58419a9bd55e13686d7dfb7c3297b360ecd
Michael Kohl <citizen428@gmail.com>
2011-04-28 00:40:32 +0200

    Undo wrongly changed key name

--- src/foreclojure/data_set.clj
+++ src/foreclojure/data_set.clj
@@ -391,7 +391,7 @@
             :times-solved 0
             :restricted ["interleave"]
             :description "Write a function which takes two sequences and returns the first item from each, then the second item from each, then the third, etc."
-            :Tags ["easy" "seqs" "core-functions"]
+            :tags ["easy" "seqs" "core-functions"]
             :tests ["(= (__ [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c))"
                     "(= (__ [1 2] [3 4 5 6]) '(1 3 2 4))"
                     "(= (__ [1 2 3 4] [5]) [1 5])"
