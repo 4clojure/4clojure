58aeb144f51c7c420822877ec0ca0b93a9bc035f
David Byrne <david.r.byrne@gmail.com>
2011-04-28 00:12:29 -0400

    Fixed typo

--- src/foreclojure/data_set.clj
+++ src/foreclojure/data_set.clj
@@ -654,7 +654,7 @@
             :title "Group a Sequence"
             :times-solved 0
             :restricted ["group-by"]
-            :description "Given a function f and a sequence s, write a function which return a map.  The keys should be the values of f applied to each item in s.  The value at each key should be a vector of corresponding items in the order they appear in s."
+            :description "Given a function f and a sequence s, write a function which returns a map.  The keys should be the values of f applied to each item in s.  The value at each key should be a vector of corresponding items in the order they appear in s."
             :tags ["medium" "seqs" "core-functions"]
             :tests ["(= (__ #(> % 5) #{1 3 6 8}) {false [1 3], true [6 8]})"
                     "(= (__ #(apply / %) [[1 2] [2 4] [4 6] [3 6]])\n   {1/2 [[1 2] [2 4] [3 6]], 2/3 [[4 6]]})"
