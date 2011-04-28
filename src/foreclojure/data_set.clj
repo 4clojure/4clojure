352f92837fd919ea4eda6f984e2612f6b3e9030b
David Byrne <david.r.byrne@gmail.com>
2011-04-28 00:26:53 -0400

    Added a test case to "Black Box Testing"

--- src/foreclojure/data_set.clj
+++ src/foreclojure/data_set.clj
@@ -676,10 +676,11 @@
             :times-solved 0
             :description "Clojure has many collection types, which act in subtly different ways. The core functions typically convert them into a uniform \"sequence\" type and work with them that way, but it can be important to understand the behavioral and performance differences so that you know which kind is appropriate for your application.<br /><br />Write a function which takes a collection and returns one of :map, :set, :list, or :vector - describing the type of collection it was given.<br />You won't be allowed to inspect their class or use the built-in predicates like list? - the point is to poke at them and understand their behavior."
             :tags ["medium" "seqs" "testing"]
-            :tests ["(= :map (__ {:a 1, :b 2}))"
-                    "(= :list (__ (range (rand-int 20))))"
-                    "(= :vector (__ [1 2 3 4 5 6]))"
-                    "(= :set (__ #{10 (rand-int 5)}))"]
+            :tests ["(= :map (__ {:a 1, :b 2}))",
+                    "(= :list (__ (range (rand-int 20))))",
+                    "(= :vector (__ [1 2 3 4 5 6]))",
+                    "(= :set (__ #{10 (rand-int 5)}))",
+                    "(= [:map :set :vector :list] (map __ [{} #{} [] ()]))"]
             :restricted (map str '[class type Class vector? sequential?
                                    list? seq? map? set? getClass])})
 
