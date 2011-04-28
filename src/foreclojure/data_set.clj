cfa02e9e1d2dc5fdcbc71666bcdd7fabdc519ebc
David Byrne <david.r.byrne@gmail.com>
2011-04-27 23:08:04 -0400

    Problems tweaked

--- src/foreclojure/data_set.clj
+++ src/foreclojure/data_set.clj
@@ -662,7 +662,7 @@
 
       (insert! :problems
            {:_id 64
-            :title "Intro to reduce"
+            :title "Intro to Reduce"
             :times-solved 0
             :description "<a href='http://clojuredocs.org/clojure_core/clojure.core/reduce'>Reduce</a> takes a 2 argument function and an optional starting value. It then applies the function to the first 2 items in the sequence (or the starting value and the first element of the sequence). In the next iteration the function will be called on the previous return value and the next item from the sequence, thus reducing the entire collection to one value. Don't worry, it's not as complicated as it sounds."
             :tags ["elementary" "seqs"]
@@ -672,9 +672,9 @@
 
       (insert! :problems
            {:_id 65
-            :title "Black box testing"
+            :title "Black Box Testing"
             :times-solved 0
-            :description "Clojure has many sequence types, which act in subtly different ways. The core functions typically convert them into a uniform \"sequence\" type and work with them that way, but it can be important to understand the behavioral and performance differences so that you know which kind is appropriate for your application.<br />Write a function which takes a collection and returns one of :map, :set, :list, or :vector - describing the type of collection it was given.<br />You won't be allowed to inspect their class or use the built-in predicates like list? - the point is to poke at them and understand their behavior."
+            :description "Clojure has many collection types, which act in subtly different ways. The core functions typically convert them into a uniform \"sequence\" type and work with them that way, but it can be important to understand the behavioral and performance differences so that you know which kind is appropriate for your application.<br /><br />Write a function which takes a collection and returns one of :map, :set, :list, or :vector - describing the type of collection it was given.<br />You won't be allowed to inspect their class or use the built-in predicates like list? - the point is to poke at them and understand their behavior."
             :tags ["medium" "seqs" "testing"]
             :tests ["(= :map (__ {:a 1, :b 2}))"
                     "(= :list (__ (range (rand-int 20))))"
