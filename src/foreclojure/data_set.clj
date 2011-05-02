cab8fa9aae742bdc3d9cd19d94dc1e7e58dc3022
David Byrne <david.r.byrne@gmail.com>
2011-05-01 22:50:30 -0400

    Changed "Analyze Tic-Tac-Toe Board" to return nil if no one has won

--- src/foreclojure/data_set.clj
+++ src/foreclojure/data_set.clj
@@ -756,15 +756,15 @@ number of prime numbers."
           {:_id 73
            :title "Analyze a Tic-Tac-Toe Board"
            :times-solved 0
-           :description "A <a href=\"http://en.wikipedia.org/wiki/Tic-tac-toe\">tic-tac-toe</a> board is represented by a two dimensional vector. X is represented by :x, O is represented by :o, and empty is represented by :e.  A player wins by placing three Xs or three Os in a horizontal, vertical, or diagonal row.  Write a function which analyzes a tic-tac-toe board and returns :x if X has won, :o if O has won, and :e if neither player has won."
+           :description "A <a href=\"http://en.wikipedia.org/wiki/Tic-tac-toe\">tic-tac-toe</a> board is represented by a two dimensional vector. X is represented by :x, O is represented by :o, and empty is represented by :e.  A player wins by placing three Xs or three Os in a horizontal, vertical, or diagonal row.  Write a function which analyzes a tic-tac-toe board and returns :x if X has won, :o if O has won, and nil if neither player has won."
            :tags ["medium" "game"]
-           :tests ["(= :e (__ [[:e :e :e]\n           [:e :e :e]\n           [:e :e :e]]))"
+           :tests ["(= nil (__ [[:e :e :e]\n            [:e :e :e]\n            [:e :e :e]]))"
 		   "(= :x (__ [[:x :e :o]\n           [:x :e :e]\n           [:x :e :o]]))"
 		   "(= :o (__ [[:e :x :e]\n           [:o :o :o]\n           [:x :e :x]]))"
-		   "(= :e (__ [[:x :e :o]\n           [:x :x :e]\n           [:o :x :o]]))"
+		   "(= nil (__ [[:x :e :o]\n            [:x :x :e]\n            [:o :x :o]]))"
 		   "(= :x (__ [[:x :e :e]\n           [:o :x :e]\n           [:o :e :x]]))"
 		   "(= :o (__ [[:x :e :o]\n           [:x :o :e]\n           [:o :e :x]]))"
-		   "(= :e (__ [[:x :o :x]\n           [:x :o :x]\n           [:o :x :o]]))"]}) 
+		   "(= nil (__ [[:x :o :x]\n            [:x :o :x]\n            [:o :x :o]]))"]}) 
       
       (insert! :problems
            {:_id 74
