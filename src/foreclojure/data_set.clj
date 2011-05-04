47a8e39a21abca15f8bb2e94fe37bc98f6a9f80a
David Byrne <david.r.byrne@gmail.com>
2011-05-03 21:08:41 -0400

    Problem added - "Intro to Trampoline"

--- src/foreclojure/data_set.clj
+++ src/foreclojure/data_set.clj
@@ -786,9 +786,16 @@ number of prime numbers."
 		   "(= (__ 40) 16)"
 		   "(= (__ 99) 60)"]}) 
 
+      (insert! :problems
+          {:_id 76
+           :title "Intro to Trampoline"
+           :times-solved 0
+           :description "The trampoline function takes a function f and a variable number of parameters.  Trampoline calls f with any parameters that were supplied.  If f returns a function, trampoline calls that function with no arguments.  This is repeated, until the return value is not a function, and then trampoline returns that non-function value.  This is useful for implementing mutually recursive algorithms in a way that won't consume the stack."
+           :tags ["medium", "recursion"]
+           :tests ["(= __\n   (letfn\n     [(foo [x y] #(bar (conj x y) y))\n      (bar [x y] (if (> (last x) 10)\n                   x\n                   #(foo x (+ 2 y))))]\n     (trampoline foo [] 1)))"]})
 
       (insert! :problems
-           {:_id 76
+           {:_id 77
             :title "Power Set"
             :times-solved 0
             :description "A power set is the set of all subsets of a given set. Given a list, produce a set of sublists while preserving the order of elements."
