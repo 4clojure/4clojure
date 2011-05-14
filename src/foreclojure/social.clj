50c642dbbbefd86fc8b7f3eac3a714556bb2d8cd
Alan Malloy <alan@malloys.org>
2011-05-13 23:29:53 -0700

    Only use the #clojure tag once an hour to prevent cluttering the channel.

--- src/foreclojure/social.clj
+++ src/foreclojure/social.clj
@@ -7,6 +7,25 @@
             [sandbar.stateful-session :as session])
   (:import java.net.URLEncoder))
 
+(defn throttled
+  "Create a version of a function which 'refuses' to be called too
+  frequently. If it has successfully been called in the last N milliseconds,
+  calls to it will return nil; if no calls have succeeded in that period, args
+  will be passed along to the base function."
+  [f ms-period]
+  (let [tracker (atom {:last-sent 0})]
+    (fn [& args]
+      (when (:accepted (swap! tracker
+                              (fn [{:keys [last-sent]}]
+                                (let [now (System/currentTimeMillis)
+                                      ok (< ms-period (- now last-sent))]
+                                  {:accepted ok
+                                   :last-sent (if ok now last-sent)}))))
+        (apply f args)))))
+
+(def clojure-hashtag (throttled (constantly " #clojure")
+                                (* 1000 60 60))) ; hourly
+
 (defn tweet-link [status & [anchor-text]]
   (str "<a href=\"http://twitter.com/home?status="
        (URLEncoder/encode status) "\">"
@@ -33,7 +52,7 @@
 
 (defn tweet-solution [id gist-url & [link-text]]
   (let [status-msg (str "Check out how I solved https://4clojure.com/problem/"
-                        id " - " gist-url " #clojure #4clojure")]
+                        id " - " gist-url " #4clojure" (clojure-hashtag))]
     (tweet-link status-msg link-text)))
 
 (def-page share-page []
