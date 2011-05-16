3f212ea5cf78056797d7b65d15b257b7764ba3c4
Alan Malloy <alan@malloys.org>
2011-05-15 21:12:17 -0700

    Let emacs reindent this whole mess

--- src/foreclojure/problems.clj
+++ src/foreclojure/problems.clj
@@ -23,11 +23,11 @@
 (defn get-problem-list 
   ([] (get-problem-list {:approved true}))
   ([criteria] 
-    (from-mongo
-    (fetch :problems
-           :only [:_id :title :tags :times-solved :user]
-           :where criteria
-           :sort {:_id 1}))))
+     (from-mongo
+      (fetch :problems
+             :only [:_id :title :tags :times-solved :user]
+             :where criteria
+             :sort {:_id 1}))))
 
 (defn next-unsolved-problem [solved-problems]
   (when-let [unsolved (->> (get-problem-list)
@@ -208,8 +208,8 @@
        [:span#message-text (session/flash-get :message)]]
       (render-golf-score)]
      (form-to {:id "run-code"} [:post *url*]
-              [:br]
-              [:br]
+       [:br]
+       [:br]
        [:p#instruct "Code which fills in the blank: "]
        (text-area {:id "code-box"
                    :spellcheck "false"}
@@ -221,8 +221,8 @@
        [:button.large {:id "run-button" :type "submit"} "Run"]
        (when-not approved
          [:span [:button.large {:id "reject-button"} "Reject"]
-                [:button.large {:id "edit-button"} "Edit"]
-                [:button.large {:id "approve-button"} "Approve"]]))]))
+          [:button.large {:id "edit-button"} "Edit"]
+          [:button.large {:id "approve-button"} "Approve"]]))]))
 
 (def-page problem-page []
   [:div.message (session/flash-get :message)]
@@ -285,22 +285,22 @@
 
 (def-page problem-submission-page []
   [:div.instructions
-    [:p "Thanks for choosing to submit a problem. Please make sure that you own the rights to the code you are submitting and that you wouldn't mind having us use the code as a 4clojure problem."]]
+   [:p "Thanks for choosing to submit a problem. Please make sure that you own the rights to the code you are submitting and that you wouldn't mind having us use the code as a 4clojure problem."]]
   (form-to {:id "problem-submission"} [:post "/problems/submit"]
-           (hidden-field :author (session/flash-get :author))
-           (hidden-field :prob-id (session/flash-get :prob-id))
-           (label :title "Problem Title")
-           (text-field :title  (session/flash-get :title))
-           (label :tags "Tags (space separated)")
-           (text-field :tags  (session/flash-get :tags))
-           (label :description "Problem Description")
-           (text-area {:id "problem-description"} :description  (session/flash-get :description))
-           [:br]
-           (label :code-box "Problem test cases. Use two underscores (__) for user input. Multiple tests ought to be on one line each.")
-           (text-area {:id "code-box" :spellcheck "false"}
-                         :code (session/flash-get :tests))
-           [:p
-             [:button.large {:id "run-button" :type "submit"} "Submit"]]))
+    (hidden-field :author (session/flash-get :author))
+    (hidden-field :prob-id (session/flash-get :prob-id))
+    (label :title "Problem Title")
+    (text-field :title  (session/flash-get :title))
+    (label :tags "Tags (space separated)")
+    (text-field :tags  (session/flash-get :tags))
+    (label :description "Problem Description")
+    (text-area {:id "problem-description"} :description  (session/flash-get :description))
+    [:br]
+    (label :code-box "Problem test cases. Use two underscores (__) for user input. Multiple tests ought to be on one line each.")
+    (text-area {:id "code-box" :spellcheck "false"}
+               :code (session/flash-get :tests))
+    [:p
+     [:button.large {:id "run-button" :type "submit"} "Submit"]]))
 
 (defn create-problem
   "create a user submitted problem"
@@ -342,8 +342,8 @@
   (if (approver? (session/session-get :user))
     (do
       (update! :problems
-        {:_id id}
-        {:$set {:approved true}})
+               {:_id id}
+               {:$set {:approved true}})
       (flash-msg (str "Problem " id " has been approved!")
                  (str "/problem/" id)))
     (flash-error "You don't have access to this page" "/problems")))
@@ -354,7 +354,7 @@
     (let [{:keys [user title description tags tests]} (get-problem id)
           email (:email (get-user user))]
       (destroy! :problems
-        {:_id id})
+                {:_id id})
       (send-email
        {:from "team@4clojure.com"
         :to [email]
@@ -374,10 +374,10 @@
   (GET "/problem/:id" [id] (code-box id))
   (GET "/problems/submit" [] (problem-submission-page))
   (POST "/problems/submit" [prob-id author title tags description code]
-        (create-problem title tags description code (when (not= "" prob-id) (Integer. prob-id)) author))
+    (create-problem title tags description code (when (not= "" prob-id) (Integer. prob-id)) author))
   (GET "/problems/unapproved" [] (unapproved-problems))
   (POST "/problem/edit" [id]
-        (edit-problem (Integer. id)))
+    (edit-problem (Integer. id)))
   (POST "/problem/approve" [id]
     (approve-problem (Integer. id)))
   (POST "/problem/reject" [id]
