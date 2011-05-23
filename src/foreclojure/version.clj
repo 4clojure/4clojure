f7e02b80dc4cc7e898f92067e2fab36a00f28427
Alan Malloy <alan@malloys.org>
2011-05-23 01:05:12 -0700

    Improve /about/version accuracy

--- src/foreclojure/version.clj
+++ src/foreclojure/version.clj
@@ -3,10 +3,14 @@
         compojure.core
         [clojure.java.shell :only [sh]]))
 
+;; fetch this at load time rather than on demand, so that it's accurate even
+;; if someone checks out a different revision to poke at without restarting
+;; the server (eg to diagnose bugs in a release)
+(def sha (:out (sh "git" "rev-parse" "--verify" "HEAD")))
+
 (def-page version []
-  (let [sha (:out (sh "git" "rev-parse" "--verify" "HEAD"))]
-    [:p "SHA: "
-     [:a {:href (str "http://github.com/dbyrne/4clojure/commit/" sha)} sha]]))
+  [:p "SHA: "
+   [:a {:href (str "http://github.com/dbyrne/4clojure/commit/" sha)} sha]])
 
 (defroutes version-routes
   (GET ["/about/version"] [] (version)))
