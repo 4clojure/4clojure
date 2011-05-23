2d51c267fe5e772c518e2b0d19c5aa4813eb02f9
Alan Malloy <alan@malloys.org>
2011-05-23 01:10:52 -0700

    Make /about/version slightly more friendly if anyone runs 4clojure from something other than a git checkout. Not sure how much value this has, but it seemed like a good idea.

--- src/foreclojure/version.clj
+++ src/foreclojure/version.clj
@@ -6,11 +6,13 @@
 ;; fetch this at load time rather than on demand, so that it's accurate even
 ;; if someone checks out a different revision to poke at without restarting
 ;; the server (eg to diagnose bugs in a release)
-(def sha (:out (sh "git" "rev-parse" "--verify" "HEAD")))
+(def sha (not-empty (:out (sh "git" "rev-parse" "--verify" "HEAD"))))
 
 (def-page version []
-  [:p "SHA: "
-   [:a {:href (str "http://github.com/dbyrne/4clojure/commit/" sha)} sha]])
+  (if sha
+    [:p "SHA: "
+     [:a {:href (str "http://github.com/dbyrne/4clojure/commit/" sha)} sha]]
+    [:p "No git repository found"]))
 
 (defroutes version-routes
   (GET ["/about/version"] [] (version)))
