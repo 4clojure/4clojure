362e6ea8f42d6f550d2daa682ee0227879c20a32
Alan Malloy <alan@malloys.org>
2011-05-22 21:37:41 -0700

    Bump bugfix version *again* - mangled last hotfix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject foreclojure "1.0.2"
+(defproject foreclojure "1.0.3"
   :description "4clojure - a website for lisp beginners"
   :dependencies [[clojure "1.2.1"]
                  [clojure-contrib "1.2.0"]
