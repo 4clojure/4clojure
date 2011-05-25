6f0523219045ef29e75b43f02ebc1882b3e7a76a
Alan Malloy <alan@malloys.org>
2011-05-25 01:57:29 -0700

    Convert URLs (which we can't really determine much information from) into Files when serving static content, so that Ring can handle Last-Modified and If-Modified-Since for us. TBH I'm no longer sure what's magical about my foreclojure.ring/resources, but it works in this context and compojure.route/resources doesn't so I'm inclined to leave it alone.

--- src/foreclojure/core.clj
+++ src/foreclojure/core.clj
@@ -1,19 +1,14 @@
 (ns foreclojure.core
   (:use compojure.core
-        [foreclojure static problems login register golf
+        [foreclojure static problems login register golf ring
          users config social version graphs mongo utils]
         ring.adapter.jetty
         somnium.congomongo
         (ring.middleware (reload :only [wrap-reload])
-                         (stacktrace :only [wrap-stacktrace])))
+                         (stacktrace :only [wrap-stacktrace])
+                         [file-info :only [wrap-file-info]]))
   (:require [compojure [route :as route] [handler :as handler]]
-            [sandbar.stateful-session :as session]
-            [ring.util.response :as response]))
-
-(defn wrap-expiry [handler seconds]
-  (fn [request]
-    (when-let [resp (handler request)]
-      (assoc-in resp [:headers "Cache-control"] (str "max-age=" seconds)))))
+            [sandbar.stateful-session :as session]))
 
 (defroutes main-routes
   (GET "/" [] (welcome-page))
@@ -26,7 +21,9 @@
   version-routes
   graph-routes
   golf-routes
-  (wrap-expiry (route/resources "/") 120) ; probably good for a couple hours
+  (-> (resources "/*")
+      (wrap-url-as-file)
+      (wrap-file-info)) ; probably good for a few minutes at least
   (route/not-found "Page not found"))
 
 (def app (-> #'main-routes
