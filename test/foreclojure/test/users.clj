1c7cab34ab792a66522a7977954fe1ee2737a86f
gigasquid <cmeier@gigasquidsoftware.com>
2011-06-04 21:37:05 -0400

    add last-solved-date

---
+++ test/foreclojure/test/users.clj
@@ -0,0 +1,7 @@
+(ns foreclojure.test.users
+  (:use [foreclojure.users] :reload)
+  (:use [clojure.test])
+  (:use [midje.sweet]))
+
+(deftest "sorting users by solved/ contributer/ solved-date/ username"
+  (fact))
