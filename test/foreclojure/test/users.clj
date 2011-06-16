e9d9022228305ac1fd0ad6f9ce6f542067a64f68
gigasquid <cmeier@gigasquidsoftware.com>
2011-06-15 22:53:03 -0400

    new sorting

--- test/foreclojure/test/users.clj
+++ test/foreclojure/test/users.clj
@@ -3,5 +3,28 @@
   (:use [clojure.test])
   (:use [midje.sweet]))
 
-(deftest "sorting users by solved/ contributer/ solved-date/ username"
-  (fact))
+
+(deftest sorting-by-solved
+  (def users-by-solved
+    [{:user "user1" :solved [1] } {:user "user2" :solved [1 2 3 4]}
+     {:user "user3" :solved [2 2] } {:user "user4" :solved [3]}])
+  (def users-sorted-by-solved (users-sort users-by-solved))
+  (fact
+   (:user (first users-sorted-by-solved)) => "user2")
+  (fact
+   (:user (last users-sorted-by-solved)) => "user1"))
+
+(deftest sorting-by-last-login-date
+  (def date-formatter (java.text.SimpleDateFormat. "MM/dd/yyyy"))
+  (def date1 (.parse date-formatter "11/01/2001"))
+  (def date2 (.parse date-formatter "8/1/2010"))
+  (def users-by-date
+    [{:user "user1" :last-login date1  } {:user "user2" :last-login date2}
+     {:user "user3" } {:user "user4" :last-login date1}])
+  (sort-by :last-login users-by-date)
+  (def users-sorted-by-date (users-sort users-by-date))
+  (fact
+   (:user (first users-sorted-by-date)) => "user2")
+  (fact
+   (:user (last users-sorted-by-date)) => "user3"))
+
