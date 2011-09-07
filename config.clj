579ae3a4dd6647478e84255bf7d1b6e74746957a
Alan Malloy <alan@malloys.org>
2011-09-07 08:38:21 -0700

    Make the list of bootstrap users configurable, and better documented

--- config.clj
+++ config.clj
@@ -8,4 +8,7 @@
  :advanced-user-count 50
  :pass ""
  :repo-url "https://github.com/4clojure/4clojure"
- :golfing-active true}
+ :golfing-active true
+ ;; this list is just for bootstrapping - the real DB is authoritative
+ :contributors ["amalloy" "dbyrne" "raynes" "cmeier" "devn" "0x89"
+                "citizen428" "daviddavis" "clinteger" "amcnamara"]}
