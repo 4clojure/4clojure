6b79fef1a4d1f610142cdb5ed4cac13868df1914
Alan Malloy <alan@malloys.org>
2011-11-20 17:22:11 -0800

    Emacs fixed some whitespace

--- test/foreclojure/test/register.clj
+++ test/foreclojure/test/register.clj
@@ -6,7 +6,7 @@
   (:use [midje.sweet])
   (:use [foreclojure.utils :only [form-row assuming flash-error]])
   (:use [somnium.congomongo :only [insert! fetch-one]]))
-  
+
 
 
 
@@ -28,37 +28,36 @@
                          (session/flash-put! :email anything) => nil]
       (fact "about do-register - good inputs"
           (do-register uname pwd pwd email) => truthy
-            (provided 
+            (provided
               (response/redirect "/") => 1))
       (fact "about do-register - userexists"
           (do-register uname pwd pwd email) => truthy
-            (provided 
+            (provided
               (fetch-one :users :where {:user uname}) => {:user "username"}
               (flash-error "/register" "User already exists") => 1))
       (fact "about do-register - username too long"
           (do-register lngname pwd pwd email) => truthy
-            (provided 
+            (provided
               (flash-error "/register" "Username must be 4-13 characters long") => 1))
       (fact "about do-register - username not alphanumeric"
           (do-register bname pwd pwd email) => truthy
-            (provided 
+            (provided
               (flash-error "/register" "Username must be alphanumeric") => 1))
       (fact "about do-register - short password"
           (do-register uname shpwd shpwd email) => truthy
-            (provided 
+            (provided
               (flash-error "/register" "Password must be at least seven characters long") => 1))
       (fact "about do-register - passwords don't match"
           (do-register uname pwd shpwd email) => truthy
-            (provided 
+            (provided
               (flash-error "/register" "Passwords don't match") => 1))
       (fact "about do-register - bad emal"
           (do-register uname pwd pwd bemail) => truthy
-            (provided 
+            (provided
               (flash-error "/register" "Please enter a valid email address") => 1))
       (fact "about do-register - email exists"
           (do-register uname pwd pwd email) => truthy
-            (provided 
+            (provided
               (fetch-one :users :where {:user uname}) => nil
               (fetch-one :users :where {:email email}) => {:user "username"}
               (flash-error "/register" "User with this email address already exists") => 1)))))
-
