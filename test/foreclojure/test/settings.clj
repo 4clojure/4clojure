38e2bf252512f635ef2ecfb4f593020226583665
ckirkendall <ckirkendall@gmail.com>
2011-11-21 21:45:18 -0500

    centralized error messages for registration and updating settings

--- test/foreclojure/test/settings.clj
+++ test/foreclojure/test/settings.clj
@@ -1,8 +1,9 @@
-               (ns foreclojure.test.settings
+(ns foreclojure.test.settings
   (:require [sandbar.stateful-session :as   session]
             [ring.util.response       :as   response])
   (:import  [org.jasypt.util.password StrongPasswordEncryptor])
   (:use [foreclojure.settings])
+  (:use [foreclojure.messages     :only [err-msgs]])
   (:use [clojure.test])
   (:use [midje.sweet])
   (:use [foreclojure.utils :only [get-user assuming flash-error flash-msg]])
@@ -41,36 +42,36 @@
           (do-update-settings! new-name old-pwd new-pwd new-pwd email false false) => truthy
             (provided 
               (fetch-one :users :where {:user new-name}) => {:user "username-new"}
-              (flash-error "/settings" "User already exists") => 1))
+              (flash-error "/settings" (err-msgs "settings.user-exists")) => 1))
       (fact "about do-update-settings! - username too long"
           (do-update-settings! lngname old-pwd new-pwd new-pwd email false false) => truthy
             (provided 
-              (flash-error "/settings" "Username must be 4-13 characters long") => 1))
+              (flash-error "/settings" (err-msgs "settings.uname-size")) => 1))
       (fact "about do-update-settings! - username not alphanumeric"
           (do-update-settings! bname old-pwd new-pwd new-pwd email false false) => truthy
             (provided 
-              (flash-error "/settings" "Username must be alphanumeric") => 1))
+              (flash-error "/settings" (err-msgs "settings.uname-alphanum")) => 1))
       (fact "about do-update-settings! - short password"
           (do-update-settings! new-name old-pwd short-pwd short-pwd email false false) => truthy
             (provided 
-              (flash-error "/settings" "New password must be at least seven characters long") => 1))
+              (flash-error "/settings" (err-msgs "settings.npwd-size")) => 1))
       (fact "about do-update-settings! - passwords don't match"
           (do-update-settings! new-name old-pwd new-pwd old-pwd email false false) => truthy
             (provided 
-              (flash-error "/settings" "New password was not entered identically twice") => 1))
+              (flash-error "/settings" (err-msgs "settings.npwd-match")) => 1))
       (fact "about do-update-settings! - old password doesn't match"
           (do-update-settings! new-name new-pwd new-pwd new-pwd email false false) => truthy
             (provided 
-              (flash-error "/settings" "Current password incorrect") => 1))
-      (fact "about do-update-settings! - bad emal"
+              (flash-error "/settings" (err-msgs "settings.pwd-incorrect")) => 1))
+      (fact "about do-update-settings! - bad email"
           (do-update-settings! new-name old-pwd new-pwd new-pwd bad-email false false) => truthy
             (provided 
-              (flash-error "/settings" "Please enter a valid email address") => 1))
+              (flash-error "/settings" (err-msgs "settings.email-invalid")) => 1))
       (fact "about do-update-settings! - email exists"
           (do-update-settings! new-name old-pwd new-pwd new-pwd email false false) => truthy
             (provided 
               ;you have to specify both because midje can't tell them apart
               (fetch-one :users :where {:user new-name}) => nil
               (fetch-one :users :where {:email email :user {:$ne old-name}}) => {:user old-name}
-              (flash-error "/settings" "User with this email address already exists") => 1)))))
+              (flash-error "/settings" (err-msgs "settings.email-exists")) => 1)))))
 
