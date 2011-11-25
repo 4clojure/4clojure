8c8b128105c7829fc3638657391e0022b3b5ec66
Alan Malloy <alan@malloys.org>
2011-11-24 23:21:00 -0800

    Version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject foreclojure "1.6.2"
+(defproject foreclojure "1.7.0"
   :description "4clojure - a website for learning Clojure"
   :dependencies [[clojure "1.2.1"]
                  [clojure-contrib "1.2.0"]
