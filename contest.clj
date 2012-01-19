6faefaf3c8f3bbb9afb3f7572f28ef92f323a808
Alan Malloy <alan@malloys.org>
2012-01-18 22:04:48 -0800

    Add contest config file

---
+++ contest.clj
@@ -0,0 +1,6 @@
+{:img {:file "images/clojure-west.png", :alt "Clojure West"}
+ :text {:banner "want-free-tickets?"
+        :main "4Clojure is proud to be supporting <a href='http://clojurewest.org/'>Clojure/West</a>, an upcoming conference targeted at Clojure developers from newcomers to veterans.
+<br /><br />
+Especially exciting is that we will be running a couple of contests to get free tickets to Clojure/West!
+The first contest will be three days long, from Friday, January 20th (12:01 AM Pacific time) to Sunday, January 22nd (11:59 PM Pacific time). If you submit the best new problem (judged by the 4Clojure site administrators) during the contest perdiod, you will receive a free ticket to Clojure/West, or a refund if you have already purchased your ticket."}}
