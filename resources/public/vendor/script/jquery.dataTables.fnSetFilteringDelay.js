db093c75845934ce015dc795e5df79dc7e973f43
Nikita Beloglazov <nikelandjelo@gmail.com>
2012-08-19 23:35:57 +0300

    Add delay to user filtering.

---
+++ resources/public/vendor/script/jquery.dataTables.fnSetFilteringDelay.js
@@ -0,0 +1,34 @@
+// Source http://datatables.net/plug-ins/api
+
+jQuery.fn.dataTableExt.oApi.fnSetFilteringDelay = function ( oSettings, iDelay ) {
+    var _that = this;
+
+    if ( iDelay === undefined ) {
+        iDelay = 250;
+    }
+
+    this.each( function ( i ) {
+        $.fn.dataTableExt.iApiIndex = i;
+        var
+            $this = this,
+            oTimerId = null,
+            sPreviousSearch = null,
+            anControl = $( 'input', _that.fnSettings().aanFeatures.f );
+
+            anControl.unbind( 'keyup' ).bind( 'keyup', function() {
+            var $$this = $this;
+
+            if (sPreviousSearch === null || sPreviousSearch != anControl.val()) {
+                window.clearTimeout(oTimerId);
+                sPreviousSearch = anControl.val();
+                oTimerId = window.setTimeout(function() {
+                    $.fn.dataTableExt.iApiIndex = i;
+                    _that.fnFilter( anControl.val() );
+                }, iDelay);
+            }
+        });
+
+        return this;
+    } );
+    return this;
+};
